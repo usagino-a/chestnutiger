887bf3dac92d54a0f739259030143007bc61fc9b
usagino-a <sometinyworld@gmail.com>
2021-03-28 16:47:16 +0900

    First commit

---
+++ js/js.js
@@ -0,0 +1,19 @@
+
+
+ $(function() {
+    $.scrollify({
+      section : "section",
+    });
+  });
+
+  $.scrollify({
+        section : "section",
+        sectionName : "section-name",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset : 0,
+        scrollbars: true,
+        before:function() {},
+        after:function() {}
+    });
+
